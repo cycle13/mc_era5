39188e5440f7ef0483eed2a2d023d868e353f7de
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-12-02 21:14:18 +0000

    minor fixes in categorisation script

--- code/categorise_and_save.py
+++ code/categorise_and_save.py
@@ -6,21 +6,26 @@ from pathlib import Path
 from textwrap import dedent
 
 import numpy as np
+
+from octant.core import TrackRun
+from octant.decor import pbar
+
 import xarray as xr
 
+from common_defs import bbox, cat_kw, columns, period, winters
 import mypaths
-from common_defs import cat_kw, winters, columns, bbox, period
-
-from octant.decor import pbar
-from octant.core import TrackRun
 
 # Select runs
 # runs2process = dict(era5=[0])  # , interim=[100, 106])
+SCRIPT = Path(__file__).stem
+lsm_paths = {
+    'era5': mypaths.era5_dir / 'lsm.nc',
+    'interim': mypaths.interim_dir / 'lsm.nc',
+}
 
 
 def parse_args(args=None):
-    """Parse command line arguments"""
-    SCRIPT = Path(__file__).stem
+    """Parse command line arguments."""
     epilog = dedent(f"""Example of use:
     ./{SCRIPT} -n era5 -r 0,3,10,11 -ll 10,20,65,85
     """)
@@ -46,9 +51,9 @@ def parse_args(args=None):
 
 
 def get_lsm(path_to_file, bbox=None, shift=False):
-    """Load land-sea mask from a file and crop a region defined by `bbox`"""
+    """Load land-sea mask from a file and crop a region defined by `bbox`."""
     # Load land-sea mask
-    lsm = xr.open_dataarray(mypaths.era5_dir / 'lsm.nc').squeeze()
+    lsm = xr.open_dataarray(path_to_file).squeeze()
     if shift:
         # Shift longitude coordinate from (0, 359) to (-180, 179)
         lsm = (lsm.assign_coords(longitude=(((lsm.longitude + 180) % 360) - 180))
@@ -63,36 +68,38 @@ def get_lsm(path_to_file, bbox=None, shift=False):
 
 
 def main(args=None):
-    """Loop over track runs and categorise them according `cat_kw`"""
+    """Loop over track runs and categorise them according `cat_kw`."""
     args = parse_args(args)
 
     if '-' in args.runs:
         _start, _end = args.runs.split('-')
-        runs = [*range(int(_start), int(_end)+1)]
+        runs = [*range(int(_start), int(_end) + 1)]
     else:  # if ',' in args.runs:
         runs = [int(i) for i in args.runs.split(',')]
     runs2process = {args.name: runs}
 
     lonlat_box = [int(i) for i in args.lonlat.split(',')]
 
-    lsm = get_lsm(mypaths.era5_dir / 'lsm.nc', bbox=lonlat_box, shift=True)
+    lsm = get_lsm(lsm_paths[args.name], bbox=lonlat_box, shift=True)
     lon2d, lat2d = np.meshgrid(lsm.longitude, lsm.latitude)
 
     for dset, run_nums in pbar(runs2process.items(), desc='dset'):
         for run_num in pbar(run_nums, leave=False, desc='run_num'):
             print(run_num)
-            TR = TrackRun()
+            full_tr = TrackRun()
             for winter in pbar(winters, desc='winter', leave=False):
                 print(winter)
                 track_res_dir = (mypaths.trackresdir / dset
                                  / f'run{run_num:03d}' / winter)
                 _tr = TrackRun(track_res_dir, columns=columns)
-                print('Begin categorise()')
-                _tr.categorise(lsm=lsm, **cat_kw)
-                TR += _tr
+                print(len(_tr))
+                if len(_tr) > 0:
+                    print('Begin categorise()')
+                    _tr.categorise(lsm=lsm, **cat_kw)
+                full_tr += _tr
 
-            TR.to_archive(mypaths.procdir /
-                          f'{dset}_run{run_num:03d}_{period}.h5')
+            full_tr.to_archive(mypaths.procdir /
+                               f'{dset}_run{run_num:03d}_{period}.h5')
 
 
 if __name__ == '__main__':
