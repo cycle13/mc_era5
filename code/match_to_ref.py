b273bc62754464d4ad5caa48a555e9882eb4ad79
Dennis Sergeev <dennis.sergeev@gmail.com>
2019-09-19 15:42:44 +0100

    generalise match_to_ref inputs; rerun NBs with new data

--- code/match_to_ref.py
+++ code/match_to_ref.py
@@ -13,17 +13,34 @@ import mypaths
 from obs_tracks_api import read_all_accacia, read_all_stars, prepare_tracks
 
 
-run_group_start = 0
-runs_grid_paths = {
-    "era5": mypaths.procdir / "runs_grid_vort_thresh_era5.json",
-    "interim": mypaths.procdir / "runs_grid_vort_thresh_interim.json",
+NAME = "stars"
+RUN_GROUP = "vort_thresh"
+
+RUN_GROUPS = {
+    "vort_thresh": {
+        "start": 0,
+        "paths": {
+            "era5": mypaths.procdir / "runs_grid_vort_thresh_era5.json",
+            "interim": mypaths.procdir / "runs_grid_vort_thresh_interim.json",
+        },
+    },
+    "tfreq": {
+        "start": 100,
+        "paths": {
+            "era5": mypaths.procdir / "runs_grid_tfreq_era5.json",
+            "interim": mypaths.procdir / "runs_grid_tfreq_interim.json",
+        },
+    },
+    "extra": {
+        "start": 200,
+        "paths": {"interim": mypaths.procdir / "runs_grid_low_thresh_interim.json"},
+    },
 }
-NAME = "accacia"
 
 match_options = [
-    dict(method="bs2000", beta=25.0),
+    # dict(method="bs2000", beta=25.0),
     dict(method="bs2000", beta=50.0),
-    dict(method="bs2000", beta=75.0),
+    # dict(method="bs2000", beta=75.0),
     dict(method="bs2000", beta=100.0),
     # dict(method="simple", thresh_dist=150.0),
     # dict(method="simple", thresh_dist=200.0),
@@ -37,12 +54,12 @@ REF_DATASETS = {
             k: (f"{k.split('_')[0]}-10-01", f"{k.split('_')[1]}-04-30") for k in winters[1:11]
         },
         "load_func": read_all_stars,
-        "filter_func": [lambda ot: ot.within_rectangle(*bbox) and ot.lifetime_h >= 3],
+        "filter_func": [lambda ot: ot.within_rectangle(*bbox) and ot.lifetime_h > 0],
     },
     "accacia": {
         "time_dict": {"accacia": ("2013-03-15", "2013-04-05")},
         "load_func": read_all_accacia,
-        "filter_func": [lambda ot: ot.within_rectangle(*bbox) and ot.lifetime_h >= 3],
+        "filter_func": [lambda ot: ot.within_rectangle(*bbox) and ot.lifetime_h > 0],
     },
 }
 
@@ -79,10 +96,10 @@ def main():
     output_dir.mkdir(exist_ok=True)
 
     # Loop over datasets, runs, subsets, matching methods
-    for dset in pbar(datasets):  # , desc="dataset"):
-        with runs_grid_paths[dset].open("r") as fp:
+    for dset in pbar(datasets[:]):  # , desc="dataset"):
+        with RUN_GROUPS[RUN_GROUP]["paths"][dset].open("r") as fp:
             runs_grid = json.load(fp)
-        for run_id, _ in pbar(enumerate(runs_grid, run_group_start)):
+        for run_id, _ in pbar(enumerate(runs_grid, RUN_GROUPS[RUN_GROUP]["start"])):
             TR = TrackRun.from_archive(mypaths.procdir / f"{dset}_run{run_id:03d}_{period}.h5")
             L.debug(mypaths.procdir / f"{dset}_run{run_id:03d}_{period}.h5")
             L.debug(TR)
